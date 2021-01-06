6a77521f201f6741578864512faa8f7649b4414f
johnbuhay <buhaydev@gmail.com>
2021-01-06 10:42:09 -0500

    refactor vauilt-import main

--- cmd/vault-import/main.go
+++ cmd/vault-import/main.go
@@ -9,14 +9,12 @@ import (
 	"github.com/dathan/go-vault-dump/pkg/load"
 	"github.com/dathan/go-vault-dump/pkg/vault"
 	"github.com/spf13/cobra"
-	"github.com/spf13/pflag"
 	"github.com/spf13/viper"
 )
 
 const (
-	debugFlag = "debug"
-	vaFlag    = "vault-addr"
-	vtFlag    = "vault-token"
+	vaFlag = "vault-addr"
+	vtFlag = "vault-token"
 )
 
 var (
@@ -44,58 +42,69 @@ var (
 			}
 
 			if err := loader.FromFile(args[0]); err != nil {
-				fmt.Fprintln(os.Stderr, err)
-				os.Exit(1)
+				exitErr(err)
 			}
 
 			return nil
 		},
 	}
+	// Verbose global var
+	Verbose bool
 )
+
+func exitErr(e error) {
+	fmt.Fprintln(os.Stderr, e)
+	os.Exit(1)
+}
 
 func init() {
 	l := len(os.Args)
 	switch {
 	case l < 2:
-		fmt.Fprintln(os.Stderr, errors.New("Not enough arguments passed, please provide path to the file"))
-		os.Exit(1)
+		exitErr(errors.New("Not enough arguments passed, please provide path to the file"))
 	case l == 2:
 		if _, err := os.Stat(os.Args[1]); err != nil {
-			fmt.Fprintln(os.Stderr, err)
-			os.Exit(1)
+			exitErr(err)
 		}
 	default:
 		fmt.Fprintln(os.Stderr, errors.New("Too many arguments passed, using the first"))
 	}
+
+	cobra.OnInitialize(initConfig)
+
+	rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is ./config.yaml)")
+	rootCmd.PersistentFlags().String(vaFlag, "https://127.0.0.1:8200", "vault url")
+	rootCmd.PersistentFlags().String(vtFlag, "", "vault token")
+	rootCmd.PersistentFlags().BoolVarP(&Verbose, "verbose", "v", false, "verbose output")
+
+	// pflag.CommandLine.ParseErrorsWhitelist.UnknownFlags = true
 }
 
-func main() {
-	pflag.String(vaFlag, "https://127.0.0.1:8200", "vault url")
-	pflag.String(vtFlag, "", "vault token")
-	pflag.String("kubeconfig", "", "location of kube config file")
-	pflag.Bool(debugFlag, false, "enables verbose messages")
-	pflag.CommandLine.ParseErrorsWhitelist.UnknownFlags = true
-	pflag.Parse()
-	viper.BindPFlags(pflag.CommandLine)
-	viper.SetConfigName("config")                // name of config file (without extension)
-	viper.SetConfigType("yaml")                  // REQUIRED if the config file does not have the extension in the name
-	viper.AddConfigPath("/etc/vault-import/")    // path to look for the config file in
-	viper.AddConfigPath("$HOME/.vault-import")   // call multiple times to add many search paths
-	viper.AddConfigPath(".")                     // optionally look for config in the working directory
+func initConfig() {
+	if cfgFile != "" {
+		viper.SetConfigFile(cfgFile) // Use config file from the flag.
+	} else {
+		viper.SetConfigName("config")              // name of config file (without extension)
+		viper.SetConfigType("yaml")                // REQUIRED if the config file does not have the extension in the name
+		viper.AddConfigPath("/etc/vault-import/")  // path to look for the config file in
+		viper.AddConfigPath("$HOME/.vault-import") // call multiple times to add many search paths
+		viper.AddConfigPath(".")                   // optionally look for config in the working directory
+	}
+
 	if err := viper.ReadInConfig(); err != nil { // Handle errors reading the config file
 		if _, ok := err.(viper.ConfigFileNotFoundError); !ok {
 			// Config file was found but another error was produced
-			fmt.Fprintln(os.Stderr, fmt.Errorf("fatal error config file: %v", err))
-			os.Exit(1)
+			exitErr(fmt.Errorf("fatal error config file: %v", err))
 		}
 	}
 
 	viper.SetEnvPrefix("VAULT_IMPORT")
 	viper.SetEnvKeyReplacer(strings.NewReplacer("-", "_", ".", "_"))
 	viper.AutomaticEnv()
+}
 
+func main() {
 	if err := rootCmd.Execute(); err != nil {
-		fmt.Fprintln(os.Stderr, err)
-		os.Exit(1)
+		exitErr(err)
 	}
 }
